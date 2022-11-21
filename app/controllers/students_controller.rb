bd6769165b536d9a7cb712f00033a11d9c0ff886
madelinedulchin <madeline.dulchin@gmail.com>
2022-11-21 17:40:21 -0500

    commit tree c4f868cf66475547a694a7d970c6fcce89c2cf0b

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
-  def index
-    students = Student.all
+  def index 
+    if params[:name]
+    students = Student.where('lower(last_name) = ? OR lower(first_name) = ?', params[:name].downcase, params[:name].downcase)
+    else 
+      students = Student.all
     render json: students
+    end
+  end
+
+
+  def show
+    student = Student.find(params[:id])
+  render json: student
   end
 
 end
